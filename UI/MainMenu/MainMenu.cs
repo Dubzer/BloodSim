d274a0729f5d56755429599557bcceffb02012bf
Dubzer <ship8577@gmail.com>
2018-02-23 19:24:07 +0300

    just a test
    lulz

---
+++ UI/MainMenu/MainMenu.cs
@@ -0,0 +1,66 @@
+﻿#region Директивы
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Diagnostics;
+#endregion
+namespace BloodSim.UI.PauseMenu
+{
+    class MainMenu
+    {
+        #region Поля
+        Texture2D TestImage;
+        private SpriteFont robotoBold42;
+        PauseMenuButton button1 = new PauseMenuButton(50, "Начать игру");
+        PauseMenuButton button2 = new PauseMenuButton(330, "Выход");
+        Rectangle backgroundRectangle;
+        Texture2D backgroundTexture;
+        Color backgroundColor;
+        Vector2 startPosition;
+        string title;
+        #endregion
+        public MainMenu(string title)
+        {
+            button1.clicked += Play;
+            button2.clicked += Exit;    
+            this.title = title;
+            backgroundColor = new Color(0, 0, 0, 0);
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            //spriteBatch.Draw(TestImage, new Vector2(0, 0), Color.White);
+            spriteBatch.Draw(backgroundTexture, backgroundRectangle, backgroundColor);
+            button1.Draw(spriteBatch);
+            button2.Draw(spriteBatch);
+            spriteBatch.DrawString(robotoBold42, title, startPosition, Color.White);
+        }
+        public void LoadContent(ContentManager Content)
+        {
+            TestImage = Content.Load<Texture2D>("TestImage");
+            button1.LoadContent(Content);
+            button2.LoadContent(Content);
+            backgroundTexture = Content.Load<Texture2D>("background");
+            robotoBold42 = Content.Load<SpriteFont>("robotoBold42");
+        }
+        public void Update(GameTime gameTime)
+        {
+            backgroundRectangle = new Rectangle(0, 0, Game1.gameWidth, Game1.gameHeight);
+            button1.Update(gameTime);
+            button2.Update(gameTime);
+            startPosition = new Vector2(Game1.gameWidth / 2 - robotoBold42.MeasureString(title).Length() / 2, Game1.gameHeight / 2 - Game1.gameWidth / 8);
+
+        }
+        void Play()
+        {
+            Game1.gameState = Game1.State.Playing;
+        }
+        void Exit()
+        {
+            Environment.Exit(0);
+        }
+
+    }
+}
---
+++ UI/MainMenu/MainMenuButton.cs
@@ -0,0 +1,90 @@
+﻿#region Директивы
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Threading;
+using System.Diagnostics;
+#endregion
+namespace BloodSim.UI.PauseMenu
+{
+    class MainMenuButton
+    {
+        #region Поля
+        private Texture2D texture;      // Текстура карточки
+        private string name;        // Название пункта меню
+        private float x, y, y_cord;
+        private Vector2 textPosition, namePosition;
+        private Rectangle rectangle;        // Ректенгл карточки
+        private Color color, defaultColor, selectedColor;        // Цвет карточки
+        private SpriteFont robotoRegular;       // Шрифты
+        private SoundEffect clickSound;     // Звук клика
+        private bool isExpanded;
+        public Action clicked;
+        #region Управление мышью
+        MouseState _currentMouseState;
+        MouseState _previousMouseState;
+        #endregion
+
+        #endregion
+        public MainMenuButton(int y, string name)
+        {
+            defaultColor = new Color(180, 180, 180, 200);       //  Цвет карточки, на которую не навели курсором
+            selectedColor = new Color(255, 255, 255, 255);      //  Цвет карточки, на которую навели курсором
+            this.y_cord = y;
+            this.name = name;
+            #region Управление мышью
+            _currentMouseState = Mouse.GetState();
+            _previousMouseState = _currentMouseState;
+            #endregion 
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        { 
+            
+            spriteBatch.Draw(texture, rectangle, color);        //  Отрисовка карточки
+            spriteBatch.DrawString(robotoRegular, name, textPosition, Color.White);
+        }
+        public void Update(GameTime gameTime)
+        {
+            y = (rectangle.Y + (rectangle.Height / 2)) - (robotoRegular.MeasureString(name).Y / 2) + y_cord;
+            textPosition = new Vector2(x + rectangle.Width / 2 - (robotoRegular.MeasureString(name).X / 2), y + rectangle.Height / 2 - (robotoRegular.MeasureString(name).Y / 2));
+            #region При нажатии на кнопку
+            if (_previousMouseState.LeftButton == ButtonState.Released && _currentMouseState.LeftButton == ButtonState.Pressed && rectangle.Intersects(Game1.cursorRectangle))
+            {
+                clickSound.Play();
+                Thread.Sleep(120);   //  
+                clicked?.Invoke();
+            }
+            #endregion
+
+
+            #region При наведении на кнопку
+            if (rectangle.Intersects(Game1.cursorRectangle))
+            {
+                color = new Color(240, 240, 240);
+            }
+            else
+            {
+                color = defaultColor;
+            }
+            #endregion
+            _previousMouseState = _currentMouseState;
+            _currentMouseState = Mouse.GetState();
+
+        }
+        public void LoadContent(ContentManager Content)
+        {
+            robotoRegular = Content.Load<SpriteFont>("robotoRegular");        //  Загрузка конента для шрита
+            rectangle = new Rectangle((int)x, (int)y, 190, 40);        // Ректенгл карты
+
+            x = 0;
+            rectangle = new Rectangle((int)x, (int)y, 190, 40);        // Ректенгл карты
+
+            clickSound = Content.Load<SoundEffect>("clickSound");       //  Загрузка конента для звука клика
+            texture = Content.Load<Texture2D>("button");
+        }
+    }
+}
