c42d83216ae13eeea9711dec1199555eceafc190
Dubzer <ship8577@gmail.com>
2018-02-24 12:31:50 +0300

    Fix
    Фикс кнопки Выход в меню в меню паузы

--- UI/PauseMenu/PauseMenu.cs
+++ UI/PauseMenu/PauseMenu.cs
@@ -13,8 +13,8 @@ namespace BloodSim.UI.PauseMenu
         #region Поля
         Texture2D TestImage;
         private SpriteFont bold42;
-        PauseMenuButton button1 = new PauseMenuButton(275, "Продолжить");
-        PauseMenuButton button2 = new PauseMenuButton(330, "Главное меню");
+        PauseMenuButton button0 = new PauseMenuButton(275, "Продолжить");
+        PauseMenuButton button1 = new PauseMenuButton(330, "Главное меню");
         Rectangle backgroundRectangle;
         Texture2D backgroundTexture;
         Color backgroundColor;
@@ -23,8 +23,8 @@ namespace BloodSim.UI.PauseMenu
         #endregion
         public PauseMenu(string title)
         {
-            button1.clicked += Play;
-
+            button0.clicked += Play;
+            button1.clicked += MainMenu;
             this.title = title;
             backgroundColor = new Color(10, 10, 10, 255);
         }
@@ -32,23 +32,23 @@ namespace BloodSim.UI.PauseMenu
         {
             //spriteBatch.Draw(TestImage, new Vector2(0, 0), Color.White);
             spriteBatch.Draw(backgroundTexture, backgroundRectangle, backgroundColor);
+            button0.Draw(spriteBatch);
             button1.Draw(spriteBatch);
-            button2.Draw(spriteBatch);
             spriteBatch.DrawString(bold42, title, startPosition, Color.White);
         }
         public void LoadContent(ContentManager Content)
         {
             TestImage = Content.Load<Texture2D>("TestImage");
+            button0.LoadContent(Content);
             button1.LoadContent(Content);
-            button2.LoadContent(Content);
             backgroundTexture = Content.Load<Texture2D>("background");
             bold42 = Content.Load<SpriteFont>("bold42");
         }
         public void Update(GameTime gameTime)
         {
             backgroundRectangle = new Rectangle(0, 0, Game1.gameWidth, Game1.gameHeight);
+            button0.Update(gameTime);
             button1.Update(gameTime);
-            button2.Update(gameTime);
             startPosition = new Vector2(Game1.gameWidth / 2 - bold42.MeasureString(title).Length() / 2, Game1.gameHeight / 2 - Game1.gameWidth / 8);
 
         }
@@ -56,6 +56,9 @@ namespace BloodSim.UI.PauseMenu
         {
             Game1.gameState = Game1.State.Playing;
         }
-
+        void MainMenu()
+        {
+            Game1.gameState = Game1.State.MainMenu;
+        }
     }
 }
